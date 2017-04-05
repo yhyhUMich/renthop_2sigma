ad2a83d753fe2fb8155fe206c48825a4d8ad8c24
Hang Yuan <hangyuan@umich.edu>
2017-04-04 22:17:48 -0400

    setup import modules

--- renthop.py
+++ renthop.py
@@ -1,2 +1,12 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+import random
+from math import exp
+import os
 
+print(os.environ['PATH'])
 print("hello world")
+
